8552f1e34ed621f83586203c939104869276d3a6
Cara Morelli <caramorelli7@gmail.com>
2018-11-18 23:24:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  return drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  return drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver() {
+  return drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+  return drivers.shift()
+}
+
+function appendDriver(name) {
+  var arrayappendDriver = [];
+  for (var i = 0; i < drivers.length; i++) {
+    arrayappendDriver.push(drivers[i]);
+  }
+  arrayappendDriver.push(name);
+  return arrayappendDriver;
+}
+
+function prependDriver(name) {
+  var arrayappendDriver = [name];
+  for (var i = 0; i < drivers.length; i++) {
+    arrayappendDriver.push(drivers[i]);
+  }
+  return arrayappendDriver;
+}
+
+function removeLastDriver() {
+  var arrayappendDriver = [];
+  for (var i = 0; i < (drivers.length - 1); i++) {
+    arrayappendDriver.push(drivers[i]);
+  }
+  return arrayappendDriver;
+}
+
+function removeFirstDriver() {
+  var arrayappendDriver = [];
+  for (var i = 1; i < drivers.length; i++) {
+    arrayappendDriver.push(drivers[i]);
+  }
+  return arrayappendDriver;
+}
