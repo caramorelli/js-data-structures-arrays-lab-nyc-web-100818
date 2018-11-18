e543e46e2791597c9b8fb0a06ee45dac011be06b
Cara Morelli <caramorelli7@gmail.com>
2018-11-18 23:05:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyPrependDriver(name) {
 }
 
 function destructivelyRemoveLastDriver() {
-  return drivers.shift
+  return drivers.pop
 }
 
 function destructivelyRemoveFirstDriver() {
